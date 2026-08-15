d1617de94ae9f2605cd316096fa8e1539358e066
nyketo <nyketo@gmail.com>
2026-08-15 13:49:47 +0300

    Add files via upload

---
+++ MyTAXO-pages-v1/MyTAXO-pages-v1/sw.js
@@ -0,0 +1,11 @@
+const CACHE='tacho561-v5';
+const APP=['./','./index.html','./tachograph-561.html','./manifest.webmanifest'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{
+    if(new URL(event.request.url).origin===location.origin){ const copy=response.clone(); caches.open(CACHE).then(cache=>cache.put(event.request,copy)); }
+    return response;
+  }).catch(()=>caches.match('./tachograph-561.html'))));
+});
